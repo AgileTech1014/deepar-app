01dacecf5108bdf091bcfd11567231b2bad4c8f5
CandyDEV1014 <lancakster512@gmail.com>
2025-03-22 04:16:52 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import * as deepar from "deepar";
+import Carousel from "./carousel.js";
+
+// Log the version. Just in case.
+console.log("Deepar version: " + deepar.version);
+
+// Top-level await is not supported.
+// So we wrap the whole code in an async function that is called immediatly.
+(async function () {
+  // Get the element you want to place DeepAR into. DeepAR will inherit its width and height from this and fill it.
+  const previewElement = document.getElementById("ar-screen");
+
+  // trigger loading progress bar animation
+  const loadingProgressBar = document.getElementById("loading-progress-bar");
+  loadingProgressBar.style.width = "100%";
+
+  // All the effects are in the public/effects folder.
+  // Here we define the order of effect files.
+  const effectList = [
+    "resources/effects/ray-ban-wayfarer.deepar",
+    "resources/effects/viking_helmet.deepar",
+    "resources/effects/MakeupLook.deepar",
+    "resources/effects/Split_View_Look.deepar",
+    "resources/effects/flower_face.deepar",
+    "resources/effects/Stallone.deepar",
+    "resources/effects/galaxy_background_web.deepar",
+    "resources/effects/Humanoid.deepar",
+    "resources/effects/Neon_Devil_Horns.deepar",
+    "resources/effects/Ping_Pong.deepar",
+    "resources/effects/Pixel_Hearts.deepar",
+    "resources/effects/Snail.deepar",
+    "resources/effects/Hope.deepar",
+    "resources/effects/Vendetta_Mask.deepar",
+    "resources/effects/Fire_Effect.deepar",
+  ];
+
+  let deepAR = null;
+
+  // Initialize DeepAR with an effect file.
+  try {
+    deepAR = await deepar.initialize({
+      licenseKey: "00b728f5a9220b967140b56a78f9895f4af19dd054aff7d075c5c1ed0399bf2855a2d42428e349bb",
+      previewElement,
+      effect: effectList[0],
+      // Removing the rootPath option will make DeepAR load the resources from the JSdelivr CDN,
+      // which is fine for development but is not recommended for production since it's not optimized for performance and can be unstable.
+      // More info here: https://docs.deepar.ai/deepar-sdk/platforms/web/tutorials/download-optimizations/#custom-deployment-of-deepar-web-resources
+      rootPath: "./resources",
+      additionalOptions: {
+        cameraConfig: {
+          // facingMode: 'environment'  // uncomment this line to use the rear camera
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("permission-denied-screen").style.display = "block";
+    return;
+  }
+
+  // Hide the loading screen.
+  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("ar-screen").style.display = "block";
+
+  window.effect = effectList[0];
+
+  const glassesCarousel = new Carousel("carousel");
+  glassesCarousel.onChange = async (value) => {
+    const loadingSpinner = document.getElementById("loading-spinner");
+
+    if (window.effect !== effectList[value]) {
+      loadingSpinner.style.display = "block";
+      await deepAR.switchEffect(effectList[value]);
+      window.effect = effectList[value];
+    }
+    loadingSpinner.style.display = "none";
+  };
+})();
